2e1e5c30e6f5064dc28fc6bbb03bcfa98f126ad3
maximka777 <maksim.borovskij@yandex.ru>
2018-11-17 21:52:49 +0300

    Added parsing fo field size

--- main.go
+++ main.go
@@ -4,12 +4,15 @@ import (
 	"flag"
 	"fmt"
 	"image"
+	"regexp"
+	"strconv"
+	"strings"
 )
 
 type Size image.Point
 
 func (s Size) String() string {
-	return fmt.Sprintf("%vx%v", s.X, s.Y)
+	return fmt.Sprintf("%v*%v", s.X, s.Y)
 }
 
 type Config struct {
@@ -22,19 +25,41 @@ func (conf Config) String() string {
 	return fmt.Sprintf("Config {popSize: %v, maxGen: %v, fieldSize: %v}", conf.popSize, conf.maxGen, conf.fieldSize)
 }
 
-func prepareConfig() Config {
+type GeneticError struct {
+	Message string
+}
+
+func (err GeneticError) Error() string {
+	return err.Message
+}
+
+func isAsterisk(r rune) bool {
+	return r == '*'
+}
+
+func prepareConfig() (Config, error) {
 	popSize := flag.Int("popSize", 10, "Population size")
 	maxGen := flag.Int("maxGen", 100, "Maximal count of generations")
-	// fieldSizeString := flag.String("fieldSize", "32*32", "Field size")
+	fieldSizeString := flag.String("fieldSize", "32*32", "Field size")
 
 	flag.Parse()
 
-	fieldSize := Size{32, 32}
+	fieldSizeRegExp := regexp.MustCompile(`\d+\*\d+`)
+	if !fieldSizeRegExp.MatchString(*fieldSizeString) {
+		return Config{}, GeneticError{"Incorrect format of field size"}
+	}
 
-	return Config{*popSize, *maxGen, fieldSize}
+	fieldSizeFields := strings.FieldsFunc(*fieldSizeString, isAsterisk)
+
+	fieldSizeWidth, _ := strconv.Atoi(fieldSizeFields[0])
+	fieldSizeHeight, _ := strconv.Atoi(fieldSizeFields[1])
+
+	fieldSize := Size{fieldSizeWidth, fieldSizeHeight}
+
+	return Config{*popSize, *maxGen, fieldSize}, nil
 }
 
 func main() {
-	config := prepareConfig()
+	config, _ := prepareConfig()
 	fmt.Println(config)
 }
